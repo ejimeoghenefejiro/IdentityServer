e8d0c95c7747e4d660b84cedbd06ca89c545c598
oghenefejiro <ejimeoghenefejiro@yahoo.com>
2020-09-21 09:54:46 +0100

    final commit

---
+++ Config.cs
@@ -0,0 +1,80 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("myresourceapi", "My Resource API")
+                {
+                    Scopes = {new Scope("apiscope")}
+                }
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                // for public api
+                new Client
+                {
+                    ClientId = "secret_client_id",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "apiscope" }
+                },
+
+                 new Client
+                {
+                    ClientId = "secret_user_client_id",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "apiscope" }
+                }
+            };
+        }
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+    {
+        new TestUser
+        {
+            SubjectId = "1",
+            Username = "user",
+            Password = "user",
+            Claims = new[]
+            {
+                new Claim("roleType", "CanReaddata")
+            }
+        },
+        new TestUser
+        {
+            SubjectId = "2",
+            Username = "admin",
+            Password = "admin",
+            Claims = new[]
+            {
+                new Claim("roleType", "CanUpdatedata")
+            }
+        }
+    };
+        }
+    }
+}
